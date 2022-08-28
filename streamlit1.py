3b0b9bf8a9d856d364201da7e851f27254aec88b
ssewa145 <109141310+ssewa145@users.noreply.github.com>
2022-08-28 18:23:26 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -32,10 +32,7 @@ def web_app():
   st.subheader("Kindely Enter The following Details in order to make a prediction")
   T2M_toc = {st.number_input("Temperature of Tocumen",0,10)
   
-  T2M_toc = T2M_toc.reshape(-1, 1)
-  
-  object = StandardScaler()
-  object.fit_transform(T2M_toc)
+  T2M_toc = (T2M_toc-27.39811537002067)/1.6761489507888916
   
   st.text_area(label='Load predition is:- ',value=T2M_toc , height= 100)
   
