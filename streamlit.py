035b39b3641184f9a9d74a97d98ed6672e4660a7
ssewa145 <109141310+ssewa145@users.noreply.github.com>
2022-08-29 23:04:52 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -131,7 +131,7 @@ def web_app():
                   
   #,index=[‘No.1’,’No.2’,’No.3’,’No.4’,'No.5','No.6','No.7','No.8','No.9','No.10','No.11','No.12','No.13','No.14','No.15','No.16','No.17,'No.18'])
 
-#df
+#dfe
   
   
   result = model_j.predict(df)
@@ -140,11 +140,11 @@ def web_app():
     st.text_area(label='Load prediction is:- ',value=result , height= 100)
   
   
-st.button("Click here to determine peak demand for the day")
-st.button("Click here to determine peak demand for the day")
+st.button("Click here to determine peak demand for the day", key=1)
+st.button("Click here to determine peak demand for the day", key=2)
 
-if st.button("Click here to determine PEAK demand for the day"):
+if st.button("Click here to determine PEAK demand for the day", key=1):
     run = peak_app()
-if st.button("Click here to determine HOURLY demand for the day"):
+if st.button("Click here to determine HOURLY demand for the day", key=2):
     run = web_app()
 
