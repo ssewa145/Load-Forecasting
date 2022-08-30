d5c62b0c22e26c47d1469b1ae4bdccbd4c96aa35
ssewa145 <109141310+ssewa145@users.noreply.github.com>
2022-08-31 01:45:25 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -39,7 +39,7 @@ def peak_app():
   ##while True :
   DOP_1 = st.date_input("Enter the date in the format dd/mm/yyyy")
   DOP = str(DOP_1)
-  DOP = datetime.datetime.strptime(DOP, "%y/%m/%d")
+  DOP = datetime.datetime.strptime(DOP, "%d/%m/%y %H:%M:%S.%f)
                        
     #try :
         #DOP = str(DOP)
