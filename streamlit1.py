bb4ef2ac0d6438d950057e5a67370c9272dd2d06
ssewa145 <109141310+ssewa145@users.noreply.github.com>
2022-08-31 02:07:43 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -37,7 +37,7 @@ def peak_app():
   """)
   st.header("The date input is required")
   ##while True :
-  DOP_1 = st.date_input("Enter the date in the format dd/mm/yyyy", datetime.datetime('%d/%m/%Y'))
+  DOP_1 = st.date_input("Enter the date in the format dd/mm/yyyy", datetime.datetime(%d/%m/%Y))
   DOP = str(DOP_1)
   DOP = datetime.datetime.strptime(DOP, '%d/%m/%Y')
                        
