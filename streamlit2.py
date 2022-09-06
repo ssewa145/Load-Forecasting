ea15fe56304ba5cb354ae0d91ed3baea8746ec74
ssewa145 <109141310+ssewa145@users.noreply.github.com>
2022-09-06 21:38:57 +0300

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -51,7 +51,7 @@ def web_app():
 
   x_peak_demand[col_names] = factors
   #print(x_peak_demand)
-  df[]'prediction'] = model_j.predict(x_peak_demand)
+  df['prediction'] = model_j.predict(x_peak_demand)
   df.drop(['date','nat_demand','T2M_toc','QV2M_toc','TQL_toc','W2M_toc','T2M_san','QV2M_san','TQL_san','W2M_san','T2M_dav','QV2M_dav','TQL_dav','W2M_dav','Holiday_ID','holiday','school','hour','month','day'])
   
   if st.button("Click here to make the Peak Demand Prediction", key=3):
