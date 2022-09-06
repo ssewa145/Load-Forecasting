8eeac7dae013ae566159a3e7e7208c5168530f82
ssewa145 <109141310+ssewa145@users.noreply.github.com>
2022-09-07 00:14:46 +0300

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -71,7 +71,7 @@ def web_app():
       #st.line_chart(df, 'datetime', ['natdemand','predictions'], width=10, height=10, use_container_width=True)
       df3 = df.copy()
       DOP_1 = df3.iat[0,20]
-      DOP_1
+      st.write(DOP_1)
       #while DOP_1 != 0:
       DOP = str(DOP_1)
       DOP = datetime.datetime.strptime(DOP, '%d/%m/%Y')
