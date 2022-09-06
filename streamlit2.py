37c8a0d3fb30449ae8e04604cea8b18cc737a10e
ssewa145 <109141310+ssewa145@users.noreply.github.com>
2022-09-07 00:35:26 +0300

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -63,7 +63,7 @@ def web_app():
       line_chart_data = df2.copy()
       line_chart_data['datetime'] = pd.to_datetime(line_chart_data['datetime'])
       line_chart_data['pickup_hour'] = line_chart_data['datetime'].dt.hour
-      hour_cross_tab = pd.crosstab(line_chart_date['pickup_hour'], line_chart_data['predictions'])
+      hour_cross_tab = pd.crosstab(line_chart_data['pickup_hour'], line_chart_data['predictions'])
       fig = px.line(hour_cross_tab)
       actual = df2['nat_demand']
       forecast = df2['predictions']
