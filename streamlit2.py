b2b5970d03c5c8b5000c5c2d291b0d2d55f7c25c
ssewa145 <109141310+ssewa145@users.noreply.github.com>
2022-09-06 21:58:58 +0300

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -6,6 +6,7 @@ import pandas as pd
 import streamlit as st
 import numpy
 import sys
+import matplotlib.pyplot as plt
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
 standard_scaler = StandardScaler()
@@ -55,7 +56,12 @@ def web_app():
   #df
   df.drop(['T2M_toc','QV2M_toc','TQL_toc','W2M_toc','T2M_san','QV2M_san','TQL_san','W2M_san','T2M_dav','QV2M_dav','TQL_dav','W2M_dav','Holiday_ID','holiday','school','hour','month','day','date'], axis=1, inplace=True)
   df
-  if st.button("Click here to make the Peak Demand Prediction", key=3):
-    st.write(df)
+  plt.plot(datetime, nat_demand, label = "nat_demand")
+  plt.plot(datetime, predictions, label = "predictions")
+  plt.xlabel('Date_Time')
+  plt.xlabel('Demand(MW)')
+  plt.show()
+  #if st.button("Click here to make the Peak Demand Prediction", key=3):
+    #st.write(df)
 
 run = web_app()
