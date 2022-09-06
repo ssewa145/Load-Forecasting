a25373d87619f1c873556ee345d06a505d03cd45
ssewa145 <109141310+ssewa145@users.noreply.github.com>
2022-09-06 21:43:09 +0300

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -39,7 +39,7 @@ def web_app():
   """)
   uploaded_file = st.file_uploader("Drag and drop the file here")
   df = pd.read_csv(uploaded_file)
-  df1 = df
+  df1 = df.copy()
   df1.drop(['datetime','date','nat_demand'], axis=1, inplace=True)
   x_peak_demand = df1.copy()
   col_names = ['T2M_toc','QV2M_toc','TQL_toc','W2M_toc','T2M_san','QV2M_san','TQL_san','W2M_san','T2M_dav','QV2M_dav','TQL_dav','W2M_dav','Holiday_ID','holiday','school','hour','month','day']
@@ -49,9 +49,9 @@ def web_app():
   x_peak_demand[col_names] = factors
   df['predictions'] = model_j.predict(x_peak_demand)
 
-  x_peak_demand[col_names] = factors
+  #x_peak_demand[col_names] = factors
   #print(x_peak_demand)
-  df['prediction'] = model_j.predict(x_peak_demand)
+  #df['prediction'] = model_j.predict(x_peak_demand)
   df
     #df.drop(['date','nat_demand','T2M_toc','QV2M_toc','TQL_toc','W2M_toc','T2M_san','QV2M_san','TQL_san','W2M_san','T2M_dav','QV2M_dav','TQL_dav','W2M_dav','Holiday_ID','holiday','school','hour','month','day'])
   
